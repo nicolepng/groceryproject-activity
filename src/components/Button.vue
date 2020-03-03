25c6c5ce0128dedc221229b3b1ceed364ea21525
nicolepng <56541613+nicolepng@users.noreply.github.com>
2020-03-03 09:52:55 +0800

    starting codes

---
+++ src/components/Button.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <p> Quantity: {{value}} </p>
+        <button v-on:click="increment"> + </button>
+        <button v-on:click="decrement"> - </button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            value: 0
+        }
+    },
+
+    methods: {
+        increment: function() {
+            return this.value = this.value + 1;
+        },
+
+        decrement: function() {
+            return this.value = this.value - 1;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    button {
+        font-family: 'Avenir', Arial, Helvetica, sans-serif;
+        text-align: center;
+        color: black;
+        font-weight: bold;
+
+    }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="headerStyle">
+    <header>
+        <h1>{{ msg }}</h1>
+        <router-link to="/" exact> List </router-link>
+        <router-link to="/Add" exact> Add Item </router-link>
+    </header>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+  data(){
+    return{
+        msg: 'Grocery List'
+        }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header{
+    background:rgb(122, 63, 100);
+    padding:10px;
+    border-style: solid;
+    border-color:black;
+    border-width: 2px;
+}
+h1{
+    color:ivory
+}
+
+</style>
